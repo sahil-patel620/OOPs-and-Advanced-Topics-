c40c5dc9dcfd6e78dae513010eda70f7cdbce490
sahil-patel620 <Sp453771@gmail.com>
2025-07-12 14:57:14 +0530

    Car class updated

--- src/OOPs/Inheritance/Car.java
+++ src/OOPs/Inheritance/Car.java
@@ -1,12 +1,17 @@
 package OOPs.Inheritance;
 
 public class Car extends Vehicle{
+
+    // method is overridden
+    void start(){
+        System.out.println("Car is starting");
+    }
     public static void main(String[] args) {
         Car maruti = new Car();
         maruti.model = "I10";
         maruti.noOfWheels = 4;
-        maruti.start();   // method inherited from Vehicle
+        maruti.start();
         System.out.println("Car Model is : "+ maruti.model);
-        System.out.println("Car have "+ maruti.noOfWheels + " wheels");
+        System.out.println("Car have "+ maruti.noOfWheels + " wheel");
     }
 }
