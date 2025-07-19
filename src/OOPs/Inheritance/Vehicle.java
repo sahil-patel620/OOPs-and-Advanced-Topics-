fcdafa697347d2d4696d512df6be229ac5f8024e
sahil-patel620 <Sp453771@gmail.com>
2025-07-19 16:53:03 +0530

    final keyword

--- src/OOPs/Inheritance/Vehicle.java
+++ src/OOPs/Inheritance/Vehicle.java
@@ -4,6 +4,12 @@ public class Vehicle {
 
     String model;
     int noOfWheels;
+    final int gears = 4;
+
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle(2);
+//        v1.gears = 8;  // cannot change final value
+    }
 
     Vehicle(){
         System.out.println("Creating a vehicle instance");
