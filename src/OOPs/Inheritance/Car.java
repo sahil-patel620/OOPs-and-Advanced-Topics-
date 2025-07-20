e84aca295134dbb3171d4ce619855d597e650940
sahil-patel620 <Sp453771@gmail.com>
2025-07-21 04:18:15 +0530

    final keyword

--- src/OOPs/Inheritance/Car.java
+++ src/OOPs/Inheritance/Car.java
@@ -22,7 +22,7 @@ public class Car extends Vehicle{
 
     public static void main(String[] args) {
         Car maruti = new Car();
-//        maruti.model = "I10";
+        maruti.model = "I10";
         System.out.println("Car have: "+ maruti.noOfWheels + " wheel");
         maruti.noOfWheels = 4;
         maruti.start();
