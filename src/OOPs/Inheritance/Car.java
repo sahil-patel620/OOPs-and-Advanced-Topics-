bcabb9509e64dcae1e2f59b46beb84907a3f3bdf
sahil-patel620 <Sp453771@gmail.com>
2025-07-22 14:22:18 +0530

    final keyword

--- src/OOPs/Inheritance/Car.java
+++ src/OOPs/Inheritance/Car.java
@@ -24,7 +24,7 @@ public class Car extends Vehicle{
         Car maruti = new Car();
         maruti.model = "I10";
         System.out.println("Car have: "+ maruti.noOfWheels + " wheel");
-        maruti.noOfWheels = 4;
+//        maruti.noOfWheels = 4;
         maruti.start();
         System.out.println("Car Model is : "+ maruti.model);
         System.out.println("Car have: "+ maruti.noOfWheels + " wheel");
