1bd1b9a7fdc8546652ec20360275d270315fdb9f
sahil-patel620 <Sp453771@gmail.com>
2025-07-30 16:12:26 +0530

    handling all  types of exception in single try-catch block

--- src/Exception_Handling/MainClass.java
+++ src/Exception_Handling/MainClass.java
@@ -1,23 +1,41 @@
 package Exception_Handling;
 
 public class MainClass {
-    
+
     public static void main(String[] args) {
         int arr[] = new int[5];
 
         System.out.println("Hello Guys ");
 
+        // try {
+        // int result = 5 /0;
+        // System.out.println(arr[8]);
+        // } catch (ArrayIndexOutOfBoundsException e) {
+        // System.out.println("Array is out of bound");
+        // }catch(ArithmeticException e){
+        // System.out.println("Exception: "+ e.getMessage());
+        // System.out.println(e.getStackTrace());
+        // System.out.println(e);
+        // }
+
+        // 2nd way of handling exception
+
+        // try {
+        // int result = 8/0;
+        // System.out.println(arr[7]);
+
+        // } catch (ArrayIndexOutOfBoundsException | ArithmeticException e) {
+        // System.out.println("Handling the exception.");
+        // }
+
+        // 3rd Way to handle exception
+
         try {
-            int result = 5 /0;
-            System.out.println(arr[8]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Array is out of bound");
-        }catch(ArithmeticException e){
-            System.out.println("Exception: "+ e.getMessage());
-             System.out.println(e.getStackTrace());
-             System.out.println(e);
+            int result = 8 / 0;
+            System.out.println(arr[7]);
+        } catch (Exception e) {
+            System.out.println("Handling all types of exceptions. ");
         }
     }
 
-    
 }
