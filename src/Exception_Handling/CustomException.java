bd0abfe24c347ecaf166725f85046d83c3aec4a1
sahil-patel620 <Sp453771@gmail.com>
2025-07-30 19:00:54 +0530

    Custom exceptions

---
+++ src/Exception_Handling/CustomException.java
@@ -0,0 +1,34 @@
+package Exception_Handling;
+
+import java.util.Scanner;
+
+public class CustomException {
+    
+    public static void main(String[] args) throws MyException {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+
+        try {
+            if (age>100) {
+                throw new MyException("Your age can't be more than 100");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        //  if (age>100) {
+        //         throw new MyException("Your age can't be more than 100");
+        //     }
+    }
+    
+
+}
+
+class MyException extends Exception{
+    public MyException(String message){
+        super(message);
+    }
+}
--- src/Exception_Handling/FinallyBlockInExcption.java
+++ src/Exception_Handling/FinallyBlockInExcption.java
@@ -2,15 +2,25 @@ package Exception_Handling;
 
 public class FinallyBlockInExcption {
     public static void main(String[] args) {
-        
+
         int arr[] = new int[6];
 
-        try{
-            System.out.println(arr[3]);
-        // }catch(Exception e ){
-        //     System.out.println(e.getMessage());
-        }finally{
-            System.out.println("I will always Execute");
+        // try{
+        // System.out.println(arr[3]);
+        // // }catch(Exception e ){
+        // // System.out.println(e.getMessage());
+        // }finally{
+        // System.out.println("I will always Execute");
+        // }
+
+        try {
+            getNumberFromArray(arr);
+        } catch (Exception e) {
+            System.out.println("Catched all the exceptions");
         }
+    }
+
+    static int getNumberFromArray(int arr[]) throws ArithmeticException {
+        return arr[8];
     }
 }
